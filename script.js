da677b5a2070168a0d69e8825216ffe1afa53368
T31K <t31kmunwong@gmail.com>
2022-09-20 10:57:29 +0800

    add date

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 function getCurrentWeek() {
-  let days  = moment(moment().toDate(), "MM-DD-YYYY").isoWeek()
-  let weekNumber = days - 24
-  document.getElementById("date").innerHTML = `Week ${weekNumber}`
+  let days = moment(moment().toDate(), "MM-DD-YYYY").isoWeek();
+  let weekNumber = days - 24;
+  document.getElementById("date").innerHTML = `Week ${weekNumber}`;
 }
 
-getCurrentWeek()
+getCurrentWeek();
+
+function getDay() {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const d = new Date();
+  let day = weekday[d.getDay()];
+
+  switch (day) {
+    case "Tuesday":
+      return "Day 1 (Tuesday)";
+    case "Thursday":
+      return "Day 2 (Thursday)";
+    case "Saturday":
+      return "Day 3 (Saturday)";
+  }
+}
+
+document.getElementById("day").innerHTML = getDay();
